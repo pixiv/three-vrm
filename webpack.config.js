7de8a3ccf7c7b4c736b75f55cfbeb3749a348631
nysd <naohiro.y@gmail.com>
2019-06-06 12:35:25 +0900

    add development module

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,53 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const base = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'index.js',
-    library: 'vrm',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-  },
-  externals: {
-    'three': {
-      commonjs: 'three',
-      commonjs2: 'three',
-      amd: 'three',
-      root: 'THREE'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        enforce: 'pre',
-        use: 'tslint-loader',
-      },
-      {
-        test: /\.ts?$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.(glsl|frag|vert)$/,
-        use: 'raw-loader'
+module.exports = (env, argv) => {
+  const PROD = argv.mode === 'production'
+  return {
+    mode: argv.mode,
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: PROD ? 'index.min.js' : `index.js`,
+      library: 'vrm',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+    },
+    externals: {
+      'three': {
+        commonjs: 'three',
+        commonjs2: 'three',
+        amd: 'three',
+        root: 'THREE'
       }
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-    modules: ['node_modules'],
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: 'src/three' }
-    ])
-  ]
-};
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          enforce: 'pre',
+          use: 'tslint-loader',
+        },
+        {
+          test: /\.ts?$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        },
+        {
+          test: /\.(glsl|frag|vert)$/,
+          use: 'raw-loader'
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.ts'],
+      modules: ['node_modules'],
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'src/three' }
+      ])
+    ]
+  }
+}
 
-module.exports = [
-  base,
-
-
-];
