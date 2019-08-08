464fca08b05b551a34a24553860fdf701dc1f843
Yutaka Obuchi <yutaka.obuchi@gmail.com>
2019-08-08 19:59:29 +0900

    VRMPartsBuilder -> VRMImporter

--- src/vrm/debug/VRMDebug.ts
+++ src/vrm/debug/VRMDebug.ts
@@ -1,73 +1,46 @@
 import * as THREE from 'three';
 import { getWorldQuaternionLite } from '../utils/math';
-import { VRM, VRMBuilder } from '../VRM';
-import { VRMPartsBuilder } from '../VRMPartsBuilder';
+import { VRM, VRMParameters } from '../VRM';
+import { VRMImporterOptions } from '../VRMImporter';
 import { DebugOption } from './DebugOption';
-import { VRMPartsBuilderDebugProxy } from './VRMPartsBuilder';
+import { VRMImporterDebug } from './VRMImporterDebug';
 
 const _v3B = new THREE.Vector3();
 const _quatA = new THREE.Quaternion();
 
-export class VRMBuilderDebug extends VRMBuilder {
-  private _option?: DebugOption;
-
-  public option(option: DebugOption): VRMBuilderDebug {
-    this._option = option;
-    return this;
-  }
-
-  public async build(gltf: THREE.GLTF): Promise<VRM> {
-    const partsBuilder = new VRMPartsBuilderDebugProxy(this._partsBuilder, this._option);
-    const vrm = new VRMDebug(partsBuilder, this._option);
-    await vrm.loadGLTF(gltf);
-    return vrm;
-  }
-}
-
 export class VRMDebug extends VRM {
-  public static get Builder(): VRMBuilderDebug {
-    return new VRMBuilderDebug();
+  public static async from(gltf: THREE.GLTF, options: VRMImporterOptions): Promise<VRM> {
+    const importer = new VRMImporterDebug(options);
+    return await importer.import(gltf);
   }
-
-  public static from(gltf: THREE.GLTF): Promise<VRM> {
-    return new VRMBuilderDebug().build(gltf);
-  }
-
-  private readonly _debugOption: DebugOption;
 
   private _faceDirectionHelper?: THREE.ArrowHelper;
   private _leftEyeDirectionHelper?: THREE.ArrowHelper;
   private _rightEyeDirectionHelper?: THREE.ArrowHelper;
 
-  constructor(partsBuilder?: VRMPartsBuilder, debugOption?: DebugOption) {
-    super(partsBuilder);
-
-    this._debugOption = debugOption || {};
-  }
-
-  public async loadGLTF(gltf: THREE.GLTF): Promise<void> {
-    await super.loadGLTF(gltf);
+  constructor(params: VRMParameters, debugOption: DebugOption = {}) {
+    super(params);
 
     // Gizmoを展開
-    if (!this._debugOption.disableBoxHelper) {
-      gltf.scene.add(new THREE.BoxHelper(gltf.scene));
+    if (!debugOption.disableBoxHelper) {
+      this.scene.add(new THREE.BoxHelper(this.scene));
     }
 
-    if (!this._debugOption.disableSkeletonHelper) {
-      gltf.scene.add(new THREE.SkeletonHelper(gltf.scene));
+    if (!debugOption.disableSkeletonHelper) {
+      this.scene.add(new THREE.SkeletonHelper(this.scene));
     }
 
-    if (!this._debugOption.disableFaceDirectionHelper) {
+    if (!debugOption.disableFaceDirectionHelper) {
       this._faceDirectionHelper = new THREE.ArrowHelper(
         new THREE.Vector3(0, 0, -1),
         new THREE.Vector3(0, 0, 0),
         0.5,
         0xff00ff,
       );
-      gltf.scene.add(this._faceDirectionHelper);
+      this.scene.add(this._faceDirectionHelper);
     }
 
-    if (this.humanBones && this.humanBones.leftEye && this.lookAt && !this._debugOption.disableLeftEyeDirectionHelper) {
+    if (this.humanBones && this.humanBones.leftEye && this.lookAt && !debugOption.disableLeftEyeDirectionHelper) {
       this._leftEyeDirectionHelper = new THREE.ArrowHelper(
         new THREE.Vector3(0, 0, -1),
         new THREE.Vector3(0, 0, 0),
@@ -75,15 +48,10 @@ export class VRMDebug extends VRM {
         0xff00ff,
         0.05,
       );
-      gltf.scene.add(this._leftEyeDirectionHelper);
+      this.scene.add(this._leftEyeDirectionHelper);
     }
 
-    if (
-      this.humanBones &&
-      this.humanBones.rightEye &&
-      this.lookAt &&
-      !this._debugOption.disableRightEyeDirectionHelper
-    ) {
+    if (this.humanBones && this.humanBones.rightEye && this.lookAt && !debugOption.disableRightEyeDirectionHelper) {
       this._rightEyeDirectionHelper = new THREE.ArrowHelper(
         new THREE.Vector3(0, 0, -1),
         new THREE.Vector3(0, 0, 0),
@@ -91,7 +59,7 @@ export class VRMDebug extends VRM {
         0xff00ff,
         0.05,
       );
-      gltf.scene.add(this._rightEyeDirectionHelper);
+      this.scene.add(this._rightEyeDirectionHelper);
     }
   }
 
