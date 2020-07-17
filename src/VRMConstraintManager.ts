8bc39c16a9ad2eacc8c8ebbeb163f6bba42c0b1c
FMS-Cat <yutaka.obuchi@gmail.com>
2020-07-17 12:48:26 +0900

    (from constraints repo) add comment

--- src/VRMConstraintManager.ts
+++ src/VRMConstraintManager.ts
@@ -37,6 +37,17 @@ export class VRMConstraintManager {
     }
   }
 
+  /**
+   * Update a constraint.
+   * If there are other constraints that are dependant, it will try to update them recursively.
+   * It might throw an error if there are circular dependencies.
+   *
+   * Intended to be used in {@link update} and {@link _processConstraint} itself recursively.
+   *
+   * @param constraint A constraint you want to update
+   * @param constraintsTried Set of constraints that are already tried to be updated
+   * @param constraintsDone Set of constraints that are already up to date
+   */
   private _processConstraint(
     constraint: VRMConstraint,
     constraintsTried: Set<VRMConstraint>,
