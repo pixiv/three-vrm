d4f18fc1440319fd259ea8734b6a3013da366368
FMS_Cat <yutaka.obuchi@gmail.com>
2020-09-15 20:08:07 +0900

    feat (VRMMetaImporter): add an option to ignore its thumbnail texture

--- src/vrm/meta/VRMMetaImporter.ts
+++ src/vrm/meta/VRMMetaImporter.ts
@@ -1,11 +1,22 @@
+import * as THREE from 'three';
 import { VRMSchema } from '../types';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { VRMMeta } from './VRMMeta';
+import { VRMMetaImporterOptions } from './VRMMetaImporterOptions';
 
 /**
  * An importer that imports a {@link VRMMeta} from a VRM extension of a GLTF.
  */
 export class VRMMetaImporter {
+  /**
+   * If `true`, it won't load its thumbnail texture ({@link VRMMeta.texture}). `false` by default.
+   */
+  public ignoreTexture: boolean;
+
+  constructor(options: VRMMetaImporterOptions) {
+    this.ignoreTexture = options.ignoreTexture ?? false;
+  }
+
   public async import(gltf: GLTF): Promise<VRMMeta | null> {
     const vrmExt: VRMSchema.VRM | undefined = gltf.parser.json.extensions?.VRM;
     if (!vrmExt) {
@@ -17,10 +28,10 @@ export class VRMMetaImporter {
       return null;
     }
 
-    const texture =
-      schemaMeta.texture != null && schemaMeta.texture !== -1
-        ? await gltf.parser.getDependency('texture', schemaMeta.texture)
-        : null;
+    let texture: THREE.Texture | null | undefined;
+    if (!this.ignoreTexture && schemaMeta.texture != null && schemaMeta.texture !== -1) {
+      texture = await gltf.parser.getDependency('texture', schemaMeta.texture);
+    }
 
     return {
       allowedUserName: schemaMeta.allowedUserName,
