19e54047f9e8361c51ab49e7171f6be8d763965c
FMS_Cat <yutaka.obuchi@gmail.com>
2020-10-28 10:58:15 +0900

    (from constraints repo) test: we can ignore cases that have non-uniform scales

--- src/tests/utils/setRandomTransform.ts
+++ src/tests/utils/setRandomTransform.ts
@@ -2,10 +2,16 @@ import * as THREE from 'three';
 import { setRandomQuaternion } from './setRandomQuaternion';
 import { Xorshift } from './Xorshift';
 
-export function setRandomTransform(object: THREE.Object3D, rng: Xorshift): THREE.Object3D {
+export function setRandomTransform(object: THREE.Object3D, rng: Xorshift): typeof object {
+  // apply random position
   object.position.set(2.0 * rng.gen() - 1.0, 2.0 * rng.gen() - 1.0, 2.0 * rng.gen() - 1.0);
+
+  // apply random rotation
   setRandomQuaternion(object.quaternion, rng);
-  object.scale.set(2.0 * rng.gen(), 2.0 * rng.gen(), 2.0 * rng.gen());
+
+  // apply random scale. scale must be uniform
+  const scale = 2.0 * rng.gen();
+  object.scale.set(scale, scale, scale);
 
   return object;
 }
