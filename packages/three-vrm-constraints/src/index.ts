a2192c8b4ad81c21a9b9886c24a92b9713daa11d
FMS_Cat <yutaka.obuchi@gmail.com>
2020-11-09 17:31:19 +0900

    change: export VRMConstraint from index

--- packages/three-vrm-constraints/src/index.ts
+++ packages/three-vrm-constraints/src/index.ts
@@ -1,5 +1,6 @@
 export * from './helpers';
 export { VRMAimConstraint } from './VRMAimConstraint';
+export { VRMConstraint } from './VRMConstraint';
 export { VRMConstraintManager } from './VRMConstraintManager';
 export { VRMConstraintSpace } from './VRMConstraintSpace';
 export { VRMPositionConstraint } from './VRMPositionConstraint';
