c6beb71e4bdd368a439fc4ffefa8b2e9479d38a1
FMS_Cat <yutaka.obuchi@gmail.com>
2021-01-28 13:41:14 +0900

    docs: trivial, fix docs comment

--- packages/three-vrm/src/vrm/utils/gltfExtractPrimitivesFromNode.ts
+++ packages/three-vrm/src/vrm/utils/gltfExtractPrimitivesFromNode.ts
@@ -28,9 +28,7 @@ function extractPrimitivesInternal(gltf: GLTF, nodeIndex: number, node: THREE.Ob
 
 /**
  * Extract primitives ( `THREE.Mesh[]` ) of a node from a loaded GLTF.
- *
  * The main purpose of this function is to distinguish primitives and children from a node that has both meshes and children.
- * It also returns informations about the node and the mesh taken from schema as by-products though.
  *
  * It utilizes the behavior that GLTFLoader adds mesh primitives to the node object ( `THREE.Group` ) first then adds its children.
  *
