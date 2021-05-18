73a4fc4e736adff69e7efb478e4abfd68e78f713
FMS_Cat <yutaka.obuchi@gmail.com>
2021-05-18 15:39:49 +0900

    fix (types, springbone): expose SpringBoneColliderGroup

--- packages/types-vrmc-springbone-1.0/src/index.ts
+++ packages/types-vrmc-springbone-1.0/src/index.ts
@@ -1,6 +1,7 @@
 export * from './SpringBone';
 export * from './SpringBoneCollider';
 export * from './SpringBoneColliderCapsule';
+export * from './SpringBoneColliderGroup';
 export * from './SpringBoneColliderShape';
 export * from './SpringBoneColliderSphere';
 export * from './SpringBoneJoint';
