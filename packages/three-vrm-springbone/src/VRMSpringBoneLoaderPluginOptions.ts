f628433dcf287952dd262136035fc6b1ebf271fc
FMS_Cat <yutaka.obuchi@gmail.com>
2021-07-01 14:12:46 +0900

    change (1.0, springbone): make options of loader plugin optional

--- packages/three-vrm-springbone/src/VRMSpringBoneLoaderPluginOptions.ts
+++ packages/three-vrm-springbone/src/VRMSpringBoneLoaderPluginOptions.ts
@@ -3,11 +3,11 @@ export interface VRMSpringBoneLoaderPluginOptions {
    * Specify an Object3D to add {@link VRMSpringBoneJointHelper} s.
    * If not specified, helper will not be created.
    */
-  jointHelperRoot: THREE.Object3D;
+  jointHelperRoot?: THREE.Object3D;
 
   /**
    * Specify an Object3D to add {@link VRMSpringBoneColliderHelper} s.
    * If not specified, helper will not be created.
    */
-  colliderHelperRoot: THREE.Object3D;
+  colliderHelperRoot?: THREE.Object3D;
 }
