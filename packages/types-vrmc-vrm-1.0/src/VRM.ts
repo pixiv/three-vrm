cf4304589d0cc38de65f3e77c42a81a27c497f36
FMS_Cat <yutaka.obuchi@gmail.com>
2021-07-05 22:40:41 +0900

    docs (1.0, schema): update doc comments of root

--- packages/types-vrmc-vrm-1.0/src/VRM.ts
+++ packages/types-vrmc-vrm-1.0/src/VRM.ts
@@ -5,19 +5,28 @@ import type { LookAt } from './LookAt';
 import type { Meta } from './Meta';
 
 export interface VRM {
+  /**
+   * Specification version of VRMC_vrm
+   */
   specVersion?: '1.0';
 
+  /**
+   * Meta informations of the VRM model
+   */
   meta?: Meta;
 
   humanoid?: Humanoid;
 
+  /**
+   * First-person perspective settings
+   */
   firstPerson?: FirstPerson;
 
+  /**
+   * Eye gaze control
+   */
   lookAt?: LookAt;
 
-  /**
-   * Definitions of expressions
-   */
   expressions?: Expression[];
 
   extensions?: { [key: string]: { [key: string]: any } };
