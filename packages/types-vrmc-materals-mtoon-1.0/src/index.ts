ba31958c1d5175ad18c76701366987c44a63ca73
FMS_Cat <yutaka.obuchi@gmail.com>
2021-07-09 12:35:41 +0900

    change (1.0, schema): MToonMaterial -> VRMCMaterialsMToon

--- packages/types-vrmc-materals-mtoon-1.0/src/index.ts
+++ packages/types-vrmc-materals-mtoon-1.0/src/index.ts
@@ -1,4 +1,4 @@
-export type { MaterialsMToon } from './MaterialsMToon';
+export type { VRMCMaterialsMToon } from './VRMCMaterialsMToon';
 export type { MaterialsMToonOutlineWidthMode } from './MaterialsMToonOutlineWidthMode';
 export type { MaterialsMToonShadingShiftTextureInfo } from './MaterialsMToonShadingShiftTextureInfo';
 export type { MaterialsMToonTextureInfo } from './MaterialsMToonTextureInfo';
