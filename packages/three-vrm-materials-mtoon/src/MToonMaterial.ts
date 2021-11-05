5e5f068533f53107840c2a3026aba84b45a782f1
0b5vr <0b5vr@0b5vr.com>
2021-11-05 12:59:03 +0900

    fix (1.0, MToon): Fix a uv issue of mtoon UV was not used If there is only emissiveMap enabled

--- packages/three-vrm-materials-mtoon/src/MToonMaterial.ts
+++ packages/three-vrm-materials-mtoon/src/MToonMaterial.ts
@@ -553,6 +553,7 @@ export class MToonMaterial extends THREE.ShaderMaterial {
     const useUvInVert = this.outlineWidthMultiplyTexture !== null;
     const useUvInFrag =
       this.map !== null ||
+      this.emissiveMap !== null ||
       this.shadeMultiplyTexture !== null ||
       this.shadingShiftTexture !== null ||
       this.rimMultiplyTexture !== null ||
