654990ebdf9ec75b856fb0401526e6c2044d893e
0b5vr <0b5vr@0b5vr.com>
2022-01-07 16:35:48 +0900

    fix (VRMHumanoid): transferBoneOrientations should process bones from parent to children

--- packages/three-vrm-core/src/humanoid/VRMHumanBoneName.ts
+++ packages/three-vrm-core/src/humanoid/VRMHumanBoneName.ts
@@ -1,31 +1,42 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
+/**
+ * The names of {@link VRMHumanoid} bone names.
+ *
+ * Ref: https://github.com/vrm-c/vrm-specification/blob/master/specification/VRMC_vrm-1.0-beta/humanoid.md
+ */
 export const VRMHumanBoneName = {
   Hips: 'hips',
   Spine: 'spine',
   Chest: 'chest',
   UpperChest: 'upperChest',
   Neck: 'neck',
+
   Head: 'head',
   LeftEye: 'leftEye',
   RightEye: 'rightEye',
   Jaw: 'jaw',
+
   LeftUpperLeg: 'leftUpperLeg',
   LeftLowerLeg: 'leftLowerLeg',
   LeftFoot: 'leftFoot',
   LeftToes: 'leftToes',
+
   RightUpperLeg: 'rightUpperLeg',
   RightLowerLeg: 'rightLowerLeg',
   RightFoot: 'rightFoot',
   RightToes: 'rightToes',
+
   LeftShoulder: 'leftShoulder',
   LeftUpperArm: 'leftUpperArm',
   LeftLowerArm: 'leftLowerArm',
   LeftHand: 'leftHand',
+
   RightShoulder: 'rightShoulder',
   RightUpperArm: 'rightUpperArm',
   RightLowerArm: 'rightLowerArm',
   RightHand: 'rightHand',
+
   LeftThumbProximal: 'leftThumbProximal',
   LeftThumbIntermediate: 'leftThumbIntermediate',
   LeftThumbDistal: 'leftThumbDistal',
@@ -41,6 +52,7 @@ export const VRMHumanBoneName = {
   LeftLittleProximal: 'leftLittleProximal',
   LeftLittleIntermediate: 'leftLittleIntermediate',
   LeftLittleDistal: 'leftLittleDistal',
+
   RightThumbProximal: 'rightThumbProximal',
   RightThumbIntermediate: 'rightThumbIntermediate',
   RightThumbDistal: 'rightThumbDistal',
