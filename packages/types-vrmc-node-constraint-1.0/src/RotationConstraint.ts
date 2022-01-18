4549e69e471a706ba79365e31b7f9882e3d8d8f4
0b5vr <0b5vr@0b5vr.com>
2022-01-18 19:28:46 +0900

    fix: update the constraint spec to the latest one

--- packages/types-vrmc-node-constraint-1.0/src/RotationConstraint.ts
+++ packages/types-vrmc-node-constraint-1.0/src/RotationConstraint.ts
@@ -1,5 +1,3 @@
-import type { ConstraintObjectSpace } from './ConstraintObjectSpace';
-
 /**
  * A constraint that links the rotation with sources.
  */
@@ -8,16 +6,6 @@ export interface RotationConstraint {
    * The index of the node constrains the node.
    */
   source: number;
-
-  /**
-   * The source node will be evaluated in this space.
-   */
-  sourceSpace?: ConstraintObjectSpace;
-
-  /**
-   * The destination node will be evaluated in this space.
-   */
-  destinationSpace?: ConstraintObjectSpace;
 
   /**
    * Axes be constrained by this constraint, in X-Y-Z order.
