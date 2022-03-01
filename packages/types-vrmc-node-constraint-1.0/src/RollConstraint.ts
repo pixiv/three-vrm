eb6a2530d099a3b58a21c757f9322fa2e98e07b7
0b5vr <0b5vr@0b5vr.com>
2022-03-01 17:50:36 +0900

    change (1.0, schema): update constraint type definitions to the newer one
    See: https://github.com/vrm-c/vrm-specification/pull/355

--- packages/types-vrmc-node-constraint-1.0/src/RollConstraint.ts
+++ packages/types-vrmc-node-constraint-1.0/src/RollConstraint.ts
@@ -1,11 +1,16 @@
 /**
- * A constraint that links the rotation with a source.
+ * A constraint that transfers a rotation around one axis of a source.
  */
-export interface RotationConstraint {
+export interface RollConstraint {
   /**
    * The index of the node constrains the node.
    */
   source: number;
+
+  /**
+   * The roll axis of the constraint.
+   */
+  rollAxis: 'X' | 'Y' | 'Z';
 
   /**
    * The weight of the constraint.
