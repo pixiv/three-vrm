55b513d4137399d7c054c6cfe3615c9dc2373304
0b5vr <0b5vr@0b5vr.com>
2022-07-21 17:28:04 +0900

    change: make VRMHumanoidRig extend VRMHumanoid
    `vrm.humanoid.rig.humanoid` was too weird

--- packages/three-vrm-core/src/humanoid/VRMHumanoidRig.ts
+++ packages/three-vrm-core/src/humanoid/VRMHumanoidRig.ts
@@ -7,43 +7,9 @@ import { VRMHumanoid } from './VRMHumanoid';
 const _v3A = new THREE.Vector3();
 const _quatA = new THREE.Quaternion();
 
-export class VRMHumanoidRig {
-  public readonly src: VRMHumanoid;
-  public readonly humanoid: VRMHumanoid;
-  public readonly root: THREE.Object3D;
-  protected readonly _parentWorldRotations: {[boneName in VRMHumanBoneName]?: THREE.Quaternion};
-  protected readonly _boneRotations: {[boneName in VRMHumanBoneName]?: THREE.Quaternion};
-
-  public constructor(humanoid: VRMHumanoid) {
-    this.src = humanoid;
-
-    const { rig, root, parentWorldRotations, boneRotations } = this._setupTransforms(humanoid);
-    this.humanoid = rig;
-    this.root = root;
-    this._parentWorldRotations = parentWorldRotations;
-    this._boneRotations = boneRotations;
-  }
-
-  /**
-   * Update this humanoid rig.
-   */
-  public update(): void {
-    VRMHumanBoneList.forEach((boneName) => {
-      const boneNode = this.src.getBoneNode(boneName);
-
-      if (boneNode != null) {
-        const rigBoneNode = this.humanoid.getBoneNode(boneName)!;
-        const parentWorldRotation = this._parentWorldRotations[boneName]!;
-        const invParentWorldRotation = _quatA.copy(parentWorldRotation).invert();
-        const boneRotation = this._boneRotations[boneName]!;
-
-        boneNode.quaternion.copy(rigBoneNode.quaternion).multiply(parentWorldRotation).premultiply(invParentWorldRotation).multiply(boneRotation);
-      }
-    });
-  }
-
-  protected _setupTransforms(humanoid: VRMHumanoid): {
-    rig: VRMHumanoid,
+export class VRMHumanoidRig extends VRMHumanoid {
+  protected static _setupTransforms(humanoid: VRMHumanoid): {
+    rigBones: VRMHumanBones,
     root: THREE.Object3D,
     parentWorldRotations: {[boneName in VRMHumanBoneName]?: THREE.Quaternion},
     boneRotations: {[boneName in VRMHumanBoneName]?: THREE.Quaternion},
@@ -110,9 +76,45 @@ export class VRMHumanoidRig {
       }
     });
 
-    // create rig humanoid
-    const rig = new VRMHumanoid(rigBones as VRMHumanBones);
+    return {
+      rigBones: rigBones as VRMHumanBones,
+      root,
+      parentWorldRotations,
+      boneRotations,
+    };
+  }
 
-    return { rig, root, parentWorldRotations, boneRotations };
+  public readonly original: VRMHumanoid;
+  public readonly root: THREE.Object3D;
+  protected readonly _parentWorldRotations: {[boneName in VRMHumanBoneName]?: THREE.Quaternion};
+  protected readonly _boneRotations: {[boneName in VRMHumanBoneName]?: THREE.Quaternion};
+
+  public constructor(humanoid: VRMHumanoid) {
+    const { rigBones, root, parentWorldRotations, boneRotations } = VRMHumanoidRig._setupTransforms(humanoid);
+
+    super(rigBones);
+
+    this.original = humanoid;
+    this.root = root;
+    this._parentWorldRotations = parentWorldRotations;
+    this._boneRotations = boneRotations;
+  }
+
+  /**
+   * Update this humanoid rig.
+   */
+  public update(): void {
+    VRMHumanBoneList.forEach((boneName) => {
+      const boneNode = this.original.getBoneNode(boneName);
+
+      if (boneNode != null) {
+        const rigBoneNode = this.getBoneNode(boneName)!;
+        const parentWorldRotation = this._parentWorldRotations[boneName]!;
+        const invParentWorldRotation = _quatA.copy(parentWorldRotation).invert();
+        const boneRotation = this._boneRotations[boneName]!;
+
+        boneNode.quaternion.copy(rigBoneNode.quaternion).multiply(parentWorldRotation).premultiply(invParentWorldRotation).multiply(boneRotation);
+      }
+    });
   }
 }
