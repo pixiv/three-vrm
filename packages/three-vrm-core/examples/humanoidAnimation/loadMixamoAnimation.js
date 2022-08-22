ccbc8c01743720128414a8886a329d3e1a6080c8
0b5vr <0b5vr@0b5vr.com>
2022-08-22 19:40:28 +0900

    refactor: mixamo animation example is now Mr.doob's Code Style™ compliant

--- packages/three-vrm-core/examples/humanoidAnimation/loadMixamoAnimation.js
+++ packages/three-vrm-core/examples/humanoidAnimation/loadMixamoAnimation.js
@@ -1,61 +1,58 @@
-/* global THREE, mixamoVRMRigMap*/
+/* global THREE, mixamoVRMRigMap */
 
 /**
- * Mixamoのアニメーションを読み込み、VRM向けに調整して返す
- * @param {string} url Mixamoのモーションが入ったURL
- * @param {VRM} vrm VRMモデル
- * @returns {Promise<THREE.AnimationClip>} AnimationClip
+ * Load Mixamo animation, convert for three-vrm use, and return it.
+ *
+ * @param {string} url A url of mixamo animation data
+ * @param {VRM} vrm A target VRM
+ * @returns {Promise<THREE.AnimationClip>} The converted AnimationClip
  */
-function loadMixamoAnimation(url, vrm) {
-  const loader = new THREE.FBXLoader(); // FBXを読み込むLoader
-  return loader.loadAsync(url).then((asset) => {
-    const clip = THREE.AnimationClip.findByName(asset.animations, 'mixamo.com'); // AnimationClipを抽出する
+function loadMixamoAnimation( url, vrm ) {
 
-    const tracks = []; // VRM用のKeyframeTrackをこの配列に格納する
+	const loader = new THREE.FBXLoader(); // A loader which loads FBX
+	return loader.loadAsync( url ).then( ( asset ) => {
 
-    clip.tracks.forEach((track) => {
-      // 各TrackをVRM向けに変換し、 `tracks` に格納する
-      const trackSplitted = track.name.split('.');
-      const mixamoRigName = trackSplitted[0];
-      const vrmBoneName = mixamoVRMRigMap[mixamoRigName];
-      const vrmNodeName = vrm.humanoid?.getNormalizedBoneNode(vrmBoneName)?.name;
+		const clip = THREE.AnimationClip.findByName( asset.animations, 'mixamo.com' ); // extract the AnimationClip
 
-      if (vrmNodeName != null) {
-        const propertyName = trackSplitted[1];
+		const tracks = []; // KeyframeTracks compatible with VRM will be added here
 
-        if (track instanceof THREE.QuaternionKeyframeTrack) {
-          tracks.push(
-            new THREE.QuaternionKeyframeTrack(
-              `${vrmNodeName}.${propertyName}`,
-              track.times,
-              track.values.map((v, i) => (vrm.meta?.metaVersion === '0' && i % 2 === 0 ? -v : v)),
-            ),
-          );
-        } else if (track instanceof THREE.VectorKeyframeTrack) {
-          let nodeName = vrmNodeName;
-          let value = track.values.map((v, i) => (vrm.meta?.metaVersion === '0' && i % 3 !== 1 ? -v : v) * 0.01);
+		clip.tracks.forEach( ( track ) => {
 
-          // MixamoのRootモーション（hips）でVRMのルートを動かす
-          // if (vrmBoneName === 'hips') {
-          //   const hips = vrm.humanoid.modelRig.getBoneNode('hips');
-          //   const root = hips.parent;
+			// Convert each tracks for VRM use, and push to `tracks`
+			const trackSplitted = track.name.split( '.' );
+			const mixamoRigName = trackSplitted[ 0 ];
+			const vrmBoneName = mixamoVRMRigMap[ mixamoRigName ];
+			const vrmNodeName = vrm.humanoid?.getNormalizedBoneNode( vrmBoneName )?.name;
 
-          //   const hipsPos = hips.getWorldPosition(new THREE.Vector3());
-          //   const rootPos = root.getWorldPosition(new THREE.Vector3());
-          //   nodeName = root.name;
+			if ( vrmNodeName != null ) {
 
-          //   const offset = hipsPos.sub(rootPos).toArray();
+				const propertyName = trackSplitted[ 1 ];
 
-          //   value = value.map((v, i) => {
-          //     return v - offset[i % 3];
-          //   });
-          // }
+				if ( track instanceof THREE.QuaternionKeyframeTrack ) {
 
-          tracks.push(new THREE.VectorKeyframeTrack(`${nodeName}.${propertyName}`, track.times, value));
-        }
-      }
-    });
+					tracks.push(
+						new THREE.QuaternionKeyframeTrack(
+							`${vrmNodeName}.${propertyName}`,
+							track.times,
+							track.values.map( ( v, i ) => ( vrm.meta?.metaVersion === '0' && i % 2 === 0 ? - v : v ) ),
+						),
+					);
 
-    return new THREE.AnimationClip('vrmAnimation', clip.duration, tracks);
-  });
+				} else if ( track instanceof THREE.VectorKeyframeTrack ) {
+
+					let nodeName = vrmNodeName;
+					let value = track.values.map( ( v, i ) => ( vrm.meta?.metaVersion === '0' && i % 3 !== 1 ? - v : v ) * 0.01 );
+
+					tracks.push( new THREE.VectorKeyframeTrack( `${nodeName}.${propertyName}`, track.times, value ) );
+
+				}
+
+			}
+
+		} );
+
+		return new THREE.AnimationClip( 'vrmAnimation', clip.duration, tracks );
+
+	} );
+
 }
--- packages/three-vrm-core/examples/humanoidAnimation/main.js
+++ packages/three-vrm-core/examples/humanoidAnimation/main.js
@@ -2,28 +2,28 @@
 
 // renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild(renderer.domElement);
+document.body.appendChild( renderer.domElement );
 
 // camera
-const camera = new THREE.PerspectiveCamera(30.0, window.innerWidth / window.innerHeight, 0.1, 20.0);
-camera.position.set(0.0, 1.0, 5.0);
+const camera = new THREE.PerspectiveCamera( 30.0, window.innerWidth / window.innerHeight, 0.1, 20.0 );
+camera.position.set( 0.0, 1.0, 5.0 );
 
 // camera controls
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.screenSpacePanning = true;
-controls.target.set(0.0, 1.0, 0.0);
+controls.target.set( 0.0, 1.0, 0.0 );
 controls.update();
 
 // scene
 const scene = new THREE.Scene();
 
 // light
-const light = new THREE.DirectionalLight(0xffffff);
-light.position.set(1.0, 1.0, 1.0).normalize();
-scene.add(light);
+const light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 1.0, 1.0, 1.0 ).normalize();
+scene.add( light );
 
 const defaultModelUrl = '../models/cube.gltf';
 
@@ -33,84 +33,118 @@ let currentAnimationUrl = undefined;
 let currentMixer = undefined;
 
 const helperRoot = new THREE.Group();
-helperRoot.renderOrder = 10000; // Helperをモデルよりも手前に描画させる
-scene.add(helperRoot);
+helperRoot.renderOrder = 10000;
+scene.add( helperRoot );
 
-function loadVRM(modelUrl) {
-  const loader = new THREE.GLTFLoader();
-  loader.crossOrigin = 'anonymous';
+function loadVRM( modelUrl ) {
 
-  helperRoot.clear();
-  loader.register((parser) => {
-    return new THREE_VRM_CORE.VRMCoreLoaderPlugin(parser, { helperRoot: helperRoot, autoUpdateHumanBones: true });
-  });
+	const loader = new THREE.GLTFLoader();
+	loader.crossOrigin = 'anonymous';
 
-  loader.load(
-    // URL of the VRM you want to load
-    modelUrl,
+	helperRoot.clear();
 
-    // called when the resource is loaded
-    (gltf) => {
-      const vrm = gltf.userData.vrmCore;
-      if (currentVrm) {
-        scene.remove(currentVrm.scene);
-      }
+	loader.register( ( parser ) => {
 
-      // put the model to the scene
-      currentVrm = vrm;
-      scene.add(vrm.scene);
+		return new THREE_VRM_CORE.VRMCoreLoaderPlugin( parser, { helperRoot: helperRoot, autoUpdateHumanBones: true } );
 
-      // HumanoidRigのRootを追加する必要がある
-      const root = vrm.humanoid.getNormalizedBoneNode('hips').parent;
-      vrm.scene.add(root);
+	} );
 
-      if (currentAnimationUrl) {
-        currentMixer = new THREE.AnimationMixer(vrm.scene); // vrmのAnimationMixerを作る
-        loadMixamoAnimation(currentAnimationUrl, vrm).then((clip) => {
-          // アニメーションを読み込む
-          currentMixer.clipAction(clip).play(); // アニメーションをMixerに適用してplay
-        });
-      }
+	loader.load(
 
-      currentVrm.scene.rotation.y = Math.PI / 4;
+		// URL of the VRM you want to load
+		modelUrl,
 
-      console.log(vrm);
-    },
+		// called when the resource is loaded
+		( gltf ) => {
 
-    // called while loading is progressing
-    (progress) => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%'),
+			const vrm = gltf.userData.vrmCore;
 
-    // called when loading has errors
-    (error) => console.error(error),
-  );
+			if ( currentVrm ) {
+
+				scene.remove( currentVrm.scene );
+
+			}
+
+			// put the model to the scene
+			currentVrm = vrm;
+			scene.add( vrm.scene );
+
+			// The root of normalized human bones needs to be in the scene
+			const root = vrm.humanoid.getNormalizedBoneNode( 'hips' ).parent;
+			vrm.scene.add( root );
+
+			if ( currentAnimationUrl ) {
+
+				loadFBX( currentAnimationUrl );
+
+			}
+
+			console.log( vrm );
+
+		},
+
+		// called while loading is progressing
+		( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
+
+		// called when loading has errors
+		( error ) => console.error( error ),
+
+	);
+
 }
-loadVRM(defaultModelUrl);
+
+loadVRM( defaultModelUrl );
+
+// mixamo animation
+function loadFBX( animationUrl ) {
+
+	currentAnimationUrl = animationUrl;
+
+	// create AnimationMixer for VRM
+	currentMixer = new THREE.AnimationMixer( currentVrm.scene );
+
+	// Load animation
+	loadMixamoAnimation( animationUrl, currentVrm ).then( ( clip ) => {
+
+		// Apply the loaded animation to mixer and play
+		currentMixer.clipAction( clip ).play();
+
+	} );
+
+}
 
 // helpers
-const gridHelper = new THREE.GridHelper(10, 10);
-scene.add(gridHelper);
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 
 // animate
 const clock = new THREE.Clock();
 
 function animate() {
-  requestAnimationFrame(animate);
 
-  const deltaTime = clock.getDelta();
+	requestAnimationFrame( animate );
 
-  if (currentMixer) {
-    // アニメーションが読み込まれていれば
-    currentMixer.update(deltaTime); // アニメーションをアップデート
-  }
+	const deltaTime = clock.getDelta();
 
-  if (currentVrm) {
-    currentVrm.update(deltaTime);
-  }
+	// if animation is loaded
+	if ( currentMixer ) {
 
-  renderer.render(scene, camera);
+		// update the animation
+		currentMixer.update( deltaTime );
+
+	}
+
+	if ( currentVrm ) {
+
+		currentVrm.update( deltaTime );
+
+	}
+
+	renderer.render( scene, camera );
+
 }
 
 animate();
@@ -118,36 +152,35 @@ animate();
 // file input
 
 // dnd handler
-window.addEventListener('dragover', function (event) {
-  event.preventDefault();
-});
+window.addEventListener( 'dragover', function ( event ) {
 
-window.addEventListener('drop', function (event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  // read given file then convert it to blob url
-  const files = event.dataTransfer.files;
-  if (!files) {
-    return;
-  }
-  const file = files[0];
-  if (!file) {
-    return;
-  }
+} );
 
-  const fileType = file.name.split('.').pop();
-  console.log(fileType);
-  const blob = new Blob([file], { type: 'application/octet-stream' });
-  const url = URL.createObjectURL(blob);
+window.addEventListener( 'drop', function ( event ) {
 
-  if (fileType === 'fbx') {
-    currentAnimationUrl = url;
-    currentMixer = new THREE.AnimationMixer(currentVrm.scene); // vrmのAnimationMixerを作る
-    loadMixamoAnimation(url, currentVrm).then((clip) => {
-      // アニメーションを読み込む
-      currentMixer.clipAction(clip).play(); // アニメーションをMixerに適用してplay
-    });
-  } else {
-    loadVRM(url);
-  }
-});
+	event.preventDefault();
+
+	// read given file then convert it to blob url
+	const files = event.dataTransfer.files;
+	if ( ! files ) return;
+
+	const file = files[ 0 ];
+	if ( ! file ) return;
+
+	const fileType = file.name.split( '.' ).pop();
+	const blob = new Blob( [ file ], { type: 'application/octet-stream' } );
+	const url = URL.createObjectURL( blob );
+
+	if ( fileType === 'fbx' ) {
+
+		loadFBX( url );
+
+	} else {
+
+		loadVRM( url );
+
+	}
+
+} );
