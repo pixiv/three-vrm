056a755f9781246eac5bcd10d45297925d2adf36
keshigomu <keshigomu@pixiv.co.jp>
2022-09-01 17:39:18 +0900

    test lint-fix
    OK

--- packages/three-vrm/examples/humanoidAnimation/main.js
+++ packages/three-vrm/examples/humanoidAnimation/main.js
@@ -2,28 +2,28 @@
 
 // renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild(renderer.domElement);
+document.body.appendChild( renderer.domElement );
 
 // camera
-const camera = new THREE.PerspectiveCamera(30.0, window.innerWidth / window.innerHeight, 0.1, 20.0);
-camera.position.set(0.0, 1.0, 5.0);
+const camera = new THREE.PerspectiveCamera( 30.0, window.innerWidth / window.innerHeight, 0.1, 20.0 );
+camera.position.set( 0.0, 1.0, 5.0 );
 
 // camera controls
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.screenSpacePanning = true;
-controls.target.set(0.0, 1.0, 0.0);
+controls.target.set( 0.0, 1.0, 0.0 );
 controls.update();
 
 // scene
 const scene = new THREE.Scene();
 
 // light
-const light = new THREE.DirectionalLight(0xffffff);
-light.position.set(1.0, 1.0, 1.0).normalize();
-scene.add(light);
+const light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 1.0, 1.0, 1.0 ).normalize();
+scene.add( light );
 
 const defaultModelUrl = '../models/three-vrm-girl.vrm';
 
@@ -34,96 +34,116 @@ let currentMixer = undefined;
 
 const helperRoot = new THREE.Group();
 helperRoot.renderOrder = 10000;
-scene.add(helperRoot);
+scene.add( helperRoot );
 
-function loadVRM(modelUrl) {
-  const loader = new THREE.GLTFLoader();
-  loader.crossOrigin = 'anonymous';
+function loadVRM( modelUrl ) {
 
-  helperRoot.clear();
+	const loader = new THREE.GLTFLoader();
+	loader.crossOrigin = 'anonymous';
 
-  loader.register((parser) => {
-    return new THREE_VRM.VRMLoaderPlugin(parser, { helperRoot: helperRoot, autoUpdateHumanBones: true });
-  });
+	helperRoot.clear();
 
-  loader.load(
-    // URL of the VRM you want to load
-    modelUrl,
+	loader.register( ( parser ) => {
 
-    // called when the resource is loaded
-    (gltf) => {
-      const vrm = gltf.userData.vrm;
+		return new THREE_VRM.VRMLoaderPlugin( parser, { helperRoot: helperRoot, autoUpdateHumanBones: true } );
 
-      if (currentVrm) {
-        scene.remove(currentVrm.scene);
+	} );
 
-        THREE_VRM.VRMUtils.deepDispose(currentVrm.scene);
-      }
+	loader.load(
+		// URL of the VRM you want to load
+		modelUrl,
 
-      // put the model to the scene
-      currentVrm = vrm;
-      scene.add(vrm.scene);
+		// called when the resource is loaded
+		( gltf ) => {
 
-      if (currentAnimationUrl) {
-        loadFBX(currentAnimationUrl);
-      }
+			const vrm = gltf.userData.vrm;
 
-      // rotate if the VRM is VRM0.0
-      THREE_VRM.VRMUtils.rotateVRM0(vrm);
+			if ( currentVrm ) {
 
-      console.log(vrm);
-    },
+				scene.remove( currentVrm.scene );
 
-    // called while loading is progressing
-    (progress) => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%'),
+				THREE_VRM.VRMUtils.deepDispose( currentVrm.scene );
 
-    // called when loading has errors
-    (error) => console.error(error),
-  );
+			}
+
+			// put the model to the scene
+			currentVrm = vrm;
+			scene.add( vrm.scene );
+
+			if ( currentAnimationUrl ) {
+
+				loadFBX( currentAnimationUrl );
+
+			}
+
+			// rotate if the VRM is VRM0.0
+			THREE_VRM.VRMUtils.rotateVRM0( vrm );
+
+			console.log( vrm );
+
+		},
+
+		// called while loading is progressing
+		( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
+
+		// called when loading has errors
+		( error ) => console.error( error ),
+	);
+
 }
 
-loadVRM(defaultModelUrl);
+loadVRM( defaultModelUrl );
 
 // mixamo animation
-function loadFBX(animationUrl) {
-  currentAnimationUrl = animationUrl;
+function loadFBX( animationUrl ) {
 
-  // create AnimationMixer for VRM
-  currentMixer = new THREE.AnimationMixer(currentVrm.scene);
+	currentAnimationUrl = animationUrl;
 
-  // Load animation
-  loadMixamoAnimation(animationUrl, currentVrm).then((clip) => {
-    // Apply the loaded animation to mixer and play
-    currentMixer.clipAction(clip).play();
-  });
+	// create AnimationMixer for VRM
+	currentMixer = new THREE.AnimationMixer( currentVrm.scene );
+
+	// Load animation
+	loadMixamoAnimation( animationUrl, currentVrm ).then( ( clip ) => {
+
+		// Apply the loaded animation to mixer and play
+		currentMixer.clipAction( clip ).play();
+
+	} );
+
 }
 
 // helpers
-const gridHelper = new THREE.GridHelper(10, 10);
-scene.add(gridHelper);
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 
 // animate
 const clock = new THREE.Clock();
 
 function animate() {
-  requestAnimationFrame(animate);
 
-  const deltaTime = clock.getDelta();
+	requestAnimationFrame( animate );
 
-  // if animation is loaded
-  if (currentMixer) {
-    // update the animation
-    currentMixer.update(deltaTime);
-  }
+	const deltaTime = clock.getDelta();
 
-  if (currentVrm) {
-    currentVrm.update(deltaTime);
-  }
+	// if animation is loaded
+	if ( currentMixer ) {
 
-  renderer.render(scene, camera);
+		// update the animation
+		currentMixer.update( deltaTime );
+
+	}
+
+	if ( currentVrm ) {
+
+		currentVrm.update( deltaTime );
+
+	}
+
+	renderer.render( scene, camera );
+
 }
 
 animate();
@@ -131,27 +151,35 @@ animate();
 // file input
 
 // dnd handler
-window.addEventListener('dragover', function (event) {
-  event.preventDefault();
-});
+window.addEventListener( 'dragover', function ( event ) {
 
-window.addEventListener('drop', function (event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  // read given file then convert it to blob url
-  const files = event.dataTransfer.files;
-  if (!files) return;
+} );
 
-  const file = files[0];
-  if (!file) return;
+window.addEventListener( 'drop', function ( event ) {
 
-  const fileType = file.name.split('.').pop();
-  const blob = new Blob([file], { type: 'application/octet-stream' });
-  const url = URL.createObjectURL(blob);
+	event.preventDefault();
 
-  if (fileType === 'fbx') {
-    loadFBX(url);
-  } else {
-    loadVRM(url);
-  }
-});
+	// read given file then convert it to blob url
+	const files = event.dataTransfer.files;
+	if ( ! files ) return;
+
+	const file = files[ 0 ];
+	if ( ! file ) return;
+
+	const fileType = file.name.split( '.' ).pop();
+	const blob = new Blob( [ file ], { type: 'application/octet-stream' } );
+	const url = URL.createObjectURL( blob );
+
+	if ( fileType === 'fbx' ) {
+
+		loadFBX( url );
+
+	} else {
+
+		loadVRM( url );
+
+	}
+
+} );
