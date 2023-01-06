a16c975de6c4dfaea7d2c666a3113e0f218e515b
0b5vr <0b5vr@0b5vr.com>
2023-01-06 17:52:38 +0900

    refactor: remove a unused utility function, linstep

--- packages/three-vrm/src/utils/math.ts
+++ packages/three-vrm/src/utils/math.ts
@@ -9,24 +9,6 @@ export function saturate(value: number): number {
   return Math.max(Math.min(value, 1.0), 0.0);
 }
 
-/**
- * Map the range of an input value from [ `min` - `max` ] to [ `0.0` - `1.0` ].
- * If input value is less than `min` , it returns `0.0`.
- * If input value is greater than `max` , it returns `1.0`.
- *
- * See also: https://threejs.org/docs/#api/en/math/Math.smoothstep
- *
- * @param x The value that will be mapped into the specified range
- * @param min Minimum value of the range
- * @param max Maximum value of the range
- */
-export function linstep(x: number, min: number, max: number): number {
-  if (x <= min) return 0;
-  if (x >= max) return 1;
-
-  return (x - min) / (max - min);
-}
-
 const _position = new THREE.Vector3();
 const _scale = new THREE.Vector3();
 const _rotation = new THREE.Quaternion();
