2f09d712311f87281c183a44460f9db0a035d7e3
0b5vr <0b5vr@0b5vr.com>
2023-12-04 15:41:28 +0900

    Revert "feature: VRMAnimation"

--- packages/three-vrm-animation/src/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  createVRMAnimationHumanoidTracks,
-  createVRMAnimationExpressionTracks,
-  createVRMAnimationLookAtTrack,
-  createVRMAnimationClip,
-} from './createVRMAnimationClip';
-export { VRMAnimation } from './VRMAnimation';
-export { VRMAnimationLoaderPlugin } from './VRMAnimationLoaderPlugin';
-export { VRMLookAtQuaternionProxy } from './VRMLookAtQuaternionProxy';
--- packages/types-vrmc-vrm-animation-1.0/src/HumanoidHumanBones.ts
+++
@@ -1,9 +0,0 @@
-import type { HumanoidHumanBone } from './HumanoidHumanBone';
-import type { HumanoidHumanBoneName } from '@pixiv/types-vrmc-vrm-1.0';
-
-/**
- * An object which maps humanoid bones to nodes.
- */
-export type HumanoidHumanBones = {
-  [key in HumanoidHumanBoneName]: HumanoidHumanBone;
-};
