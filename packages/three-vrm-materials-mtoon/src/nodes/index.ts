efacd1663d29be4b2dbe65159a99d51aad28b5af
0b5vr <0b5vr@0b5vr.com>
2024-03-25 19:09:38 +0900

    feature!: MToonNodeMaterialLoaderPlugin
    If you want to use three-vrm on WebGPU, use `MToonNodeMaterialLoaderPlugin` by specifying the loader in the option of `VRMLoaderPlugin`.
    See the `webgpu-dnd.html` example for details
    
    BREAKING: MToonMaterialLoaderPlugin might put materials other than MToonMaterial (namely, MToonNodeMaterial in this case) to `gltf.userData.vrmMToonMaterials`.
    This shouldn't affect many users since the interface of MToonNodeMaterial is almost identical to MToonMaterial

--- packages/three-vrm-materials-mtoon/src/nodes/index.ts
+++ packages/three-vrm-materials-mtoon/src/nodes/index.ts
@@ -1,3 +1,4 @@
 export { MToonAnimatedUVNode } from './MToonAnimatedUVNode';
 export { MToonLightingModel } from './MToonLightingModel';
 export { MToonNodeMaterial } from './MToonNodeMaterial';
+export { MToonNodeMaterialLoaderPlugin } from './MToonNodeMaterialLoaderPlugin';
