150d637a61e683a3d26f35a7c354da2efa26fd66
0b5vr <0b5vr@0b5vr.com>
2024-07-23 20:28:56 +0900

    BREAKING feature: preparing for Three.js r167
    import WebGPU / NodeMaterial stuff from `three/webgpu`
    See: https://github.com/mrdoob/three.js/pull/28650
    
    Three.js r167 is not released yet!
    I tested the behavior by using `yarn link` on `three` and `@types/three`
    
    importmaps for Three.js in webgpu examples are temporarily replaced to node_modules, please change them back before merging
    
    Some codes inside MToonNodeMaterial emit type errors because of recent @types/three changes
    I'm asking Methuselar96 why the change is made
    See: https://github.com/three-types/three-ts-types/pull/1023#issuecomment-2244842594

--- packages/three-vrm-materials-mtoon/src/nodes/MToonLightingModel.ts
+++ packages/three-vrm-materials-mtoon/src/nodes/MToonLightingModel.ts
@@ -1,4 +1,4 @@
-import * as Nodes from 'three/addons/nodes/Nodes.js';
+import * as THREE from 'three/webgpu';
 import {
   matcap,
   parametricRim,
@@ -11,15 +11,15 @@ import {
 
 // TODO: 0% confidence about function types.
 
-const linearstep = Nodes.tslFn(
+const linearstep = THREE.tslFn(
   ({
     a,
     b,
     t,
   }: {
-    a: Nodes.ShaderNodeObject<Nodes.Node>;
-    b: Nodes.ShaderNodeObject<Nodes.Node>;
-    t: Nodes.ShaderNodeObject<Nodes.Node>;
+    a: THREE.ShaderNodeObject<THREE.Node>;
+    b: THREE.ShaderNodeObject<THREE.Node>;
+    t: THREE.ShaderNodeObject<THREE.Node>;
   }) => {
     const top = t.sub(a);
     const bottom = b.sub(a);
@@ -30,12 +30,12 @@ const linearstep = Nodes.tslFn(
 /**
  * Convert NdotL into toon shading factor using shadingShift and shadingToony
  */
-const getShading = Nodes.tslFn(({ dotNL }: { dotNL: Nodes.ShaderNodeObject<Nodes.Node> }) => {
+const getShading = THREE.tslFn(({ dotNL }: { dotNL: THREE.ShaderNodeObject<THREE.Node> }) => {
   const shadow = 1.0; // TODO
 
-  const feather = Nodes.float(1.0).sub(shadingToony);
+  const feather = THREE.float(1.0).sub(shadingToony);
 
-  let shading: Nodes.ShaderNodeObject<Nodes.Node> = dotNL.add(shadingShift);
+  let shading: THREE.ShaderNodeObject<THREE.Node> = dotNL.add(shadingShift);
   shading = linearstep({
     a: feather.negate(),
     b: feather,
@@ -48,28 +48,28 @@ const getShading = Nodes.tslFn(({ dotNL }: { dotNL: Nodes.ShaderNodeObject<Nodes
 /**
  * Mix diffuseColor and shadeColor using shading factor and light color
  */
-const getDiffuse = Nodes.tslFn(
+const getDiffuse = THREE.tslFn(
   ({
     shading,
     lightColor,
   }: {
-    shading: Nodes.ShaderNodeObject<Nodes.Node>;
-    lightColor: Nodes.ShaderNodeObject<Nodes.Node>;
+    shading: THREE.ShaderNodeObject<THREE.Node>;
+    lightColor: THREE.ShaderNodeObject<THREE.Node>;
   }) => {
-    const diffuseColor = Nodes.mix(shadeColor, Nodes.diffuseColor, shading);
-    const col = lightColor.mul(Nodes.BRDF_Lambert({ diffuseColor }));
+    const diffuseColor = THREE.mix(shadeColor, THREE.diffuseColor, shading);
+    const col = lightColor.mul(THREE.BRDF_Lambert({ diffuseColor }));
 
     return col;
   },
 );
 
-export class MToonLightingModel extends Nodes.LightingModel {
+export class MToonLightingModel extends THREE.LightingModel {
   constructor() {
     super();
   }
 
-  direct({ lightDirection, lightColor, reflectedLight }: Nodes.LightingModelDirectInput) {
-    const dotNL = Nodes.transformedNormalView.dot(lightDirection).clamp(-1.0, 1.0);
+  direct({ lightDirection, lightColor, reflectedLight }: THREE.LightingModelDirectInput) {
+    const dotNL = THREE.transformedNormalView.dot(lightDirection).clamp(-1.0, 1.0);
 
     // toon diffuse
     const shading = getShading({
@@ -79,47 +79,47 @@ export class MToonLightingModel extends Nodes.LightingModel {
     // Unable to use `addAssign` in the current @types/three, we use `assign` and `add` instead
     // TODO: Fix the `addAssign` issue from the `@types/three` side
 
-    (reflectedLight.directDiffuse as Nodes.ShaderNodeObject<Nodes.Node>).assign(
-      (reflectedLight.directDiffuse as Nodes.ShaderNodeObject<Nodes.Node>).add(
+    (reflectedLight.directDiffuse as THREE.ShaderNodeObject<THREE.Node>).assign(
+      (reflectedLight.directDiffuse as THREE.ShaderNodeObject<THREE.Node>).add(
         getDiffuse({
           shading,
-          lightColor: lightColor as Nodes.ShaderNodeObject<Nodes.Node>,
+          lightColor: lightColor as THREE.ShaderNodeObject<THREE.Node>,
         }),
       ),
     );
 
     // rim
-    (reflectedLight.directSpecular as Nodes.ShaderNodeObject<Nodes.Node>).assign(
-      (reflectedLight.directSpecular as Nodes.ShaderNodeObject<Nodes.Node>).add(
+    (reflectedLight.directSpecular as THREE.ShaderNodeObject<THREE.Node>).assign(
+      (reflectedLight.directSpecular as THREE.ShaderNodeObject<THREE.Node>).add(
         parametricRim
           .add(matcap)
           .mul(rimMultiply)
-          .mul(Nodes.mix(Nodes.vec3(0.0), Nodes.BRDF_Lambert({ diffuseColor: lightColor }), rimLightingMix)),
+          .mul(THREE.mix(THREE.vec3(0.0), THREE.BRDF_Lambert({ diffuseColor: lightColor }), rimLightingMix)),
       ),
     );
   }
 
-  indirectDiffuse({ irradiance, reflectedLight }: Nodes.LightingModelIndirectInput) {
+  indirectDiffuse({ irradiance, reflectedLight }: THREE.LightingModelIndirectInput) {
     // indirect irradiance
-    (reflectedLight.indirectDiffuse as Nodes.ShaderNodeObject<Nodes.Node>).assign(
-      (reflectedLight.indirectDiffuse as Nodes.ShaderNodeObject<Nodes.Node>).add(
-        (irradiance as Nodes.ShaderNodeObject<Nodes.Node>).mul(
-          Nodes.BRDF_Lambert({
-            diffuseColor: Nodes.diffuseColor,
+    (reflectedLight.indirectDiffuse as THREE.ShaderNodeObject<THREE.Node>).assign(
+      (reflectedLight.indirectDiffuse as THREE.ShaderNodeObject<THREE.Node>).add(
+        (irradiance as THREE.ShaderNodeObject<THREE.Node>).mul(
+          THREE.BRDF_Lambert({
+            diffuseColor: THREE.diffuseColor,
           }),
         ),
       ),
     );
   }
 
-  indirectSpecular({ reflectedLight }: Nodes.LightingModelIndirectInput) {
+  indirectSpecular({ reflectedLight }: THREE.LightingModelIndirectInput) {
     // rim
-    (reflectedLight.indirectSpecular as Nodes.ShaderNodeObject<Nodes.Node>).assign(
-      (reflectedLight.indirectSpecular as Nodes.ShaderNodeObject<Nodes.Node>).add(
+    (reflectedLight.indirectSpecular as THREE.ShaderNodeObject<THREE.Node>).assign(
+      (reflectedLight.indirectSpecular as THREE.ShaderNodeObject<THREE.Node>).add(
         parametricRim
           .add(matcap)
           .mul(rimMultiply)
-          .mul(Nodes.mix(Nodes.vec3(1.0), Nodes.vec3(0.0), rimLightingMix)),
+          .mul(THREE.mix(THREE.vec3(1.0), THREE.vec3(0.0), rimLightingMix)),
       ),
     );
   }
