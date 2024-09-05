cebaa7bcf57eadf4c232c9e502a9238daed24d9c
0b5vr <0b5vr@0b5vr.com>
2024-09-05 15:44:56 +0900

    deps: Bump @types/three to r168
    Update type definition of `FnCompat` according the latest `@types/three`

--- packages/three-vrm-materials-mtoon/src/nodes/utils/FnCompat.ts
+++ packages/three-vrm-materials-mtoon/src/nodes/utils/FnCompat.ts
@@ -8,12 +8,12 @@ import * as THREE from 'three/webgpu';
  * See: https://github.com/mrdoob/three.js/pull/29064
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const FnCompat: typeof THREE.tslFn = (jsFunc: any) => {
+export const FnCompat: typeof THREE.Fn = (jsFunc: any) => {
   // COMPAT r168: `tslFn()` has been renamed to `Fn()`
   // See: https://github.com/mrdoob/three.js/pull/29064
   const threeRevision = parseInt(THREE.REVISION, 10);
   if (threeRevision >= 168) {
-    return (THREE as any).Fn(jsFunc);
+    return THREE.Fn(jsFunc);
   } else {
     return (THREE as any).tslFn(jsFunc);
   }
