bc70c3c91bed16e7ae04e4f3bd8240fbf11257c0
ke456-png <ke456-png@users.noreply.github.com>
2026-07-09 07:46:01 +0000

    deploy: ff42fae4fcee1fcbca2cd262c7f5f8cbddeaf5ab

--- packages/three-vrm-materials-v0compat/lib/three-vrm-materials-v0compat.module.js
+++ packages/three-vrm-materials-v0compat/lib/three-vrm-materials-v0compat.module.js
@@ -1,5 +1,5 @@
 /*!
- * @pixiv/three-vrm-materials-v0compat v3.5.4
+ * @pixiv/three-vrm-materials-v0compat v3.5.5
  * VRM0.0 materials compatibility layer plugin for @pixiv/three-vrm
  *
  * Copyright (c) 2019-2026 pixiv Inc.
